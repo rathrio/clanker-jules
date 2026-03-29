c493ac71f5ae665cae1492d25b8832e236b8335a
Rathesan Iyadurai <rad.iyadurai@gmail.com>
2026-03-29 21:46:57 +0200

    webfetch

--- tool.rb
+++ tool.rb
@@ -196,3 +196,36 @@ class EditTool
     "Edited #{path}"
   end
 end
+
+class WebfetchTool
+  include Tool
+
+  def self.description
+    'Fetch the contents of a webpage and return it as clean Markdown'
+  end
+
+  def self.render_execution(args)
+    "Fetching webpage: #{args['url']}"
+  end
+
+  param name: 'url', type: String, description: 'The URL of the webpage to fetch'
+  def call(params)
+    require 'net/http'
+    require 'uri'
+
+    url = params.fetch('url')
+    jina_url = URI("https://r.jina.ai/#{url}")
+    
+    begin
+      response = Net::HTTP.get_response(jina_url)
+      
+      if response.is_a?(Net::HTTPSuccess)
+        response.body
+      else
+        "Error: Failed to fetch webpage. HTTP Status: #{response.code}"
+      end
+    rescue => e
+      "Error fetching webpage: #{e.message}"
+    end
+  end
+end
